62a41d42b51784726e5da6b6a388e69d0c600387
Vasileios Chouliaras <vasilis.chouliaras@gmail.com>
2018-10-02 15:26:53 +0300

    Check for various platforms

--- src/Styling.js
+++ src/Styling.js
@@ -1,14 +1,29 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 
-class Styling extends Component {
+class PlatformSpecific extends Component {
+
+  componentDidMount() {
+    console.log('Platform.Version ', Platform.Version)
+  }
+
   render() {
     return (
       <View>
-        <Text>Welcome to React Native styling</Text>
+        <Text style={styles.text}>Welcome to React Native PlatformSpecific</Text>
       </View>
     )
   }
 }
 
-export default Styling
+const styles = StyleSheet.create({
+  text: {
+    fontSize: Platform.OS === 'ios' ? 30 : 15,
+    ...Platform.select({
+      ios: { fontSize: 60 },
+      android: {fontSize: 15}
+    })
+  }
+})
+
+export default PlatformSpecific
