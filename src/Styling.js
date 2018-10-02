9017bac46bc6978f6f47bb291610559b4635e21a
Vasileios Chouliaras <vasilis.chouliaras@gmail.com>
2018-10-02 16:31:57 +0300

    Http request with async/await

--- src/Styling.js
+++ src/Styling.js
@@ -3,19 +3,33 @@ import {Platform, StyleSheet, Text, View} from 'react-native';
 
 class HttpExample extends Component {
 
-  state = {data: ''}
+  state = { data: '' }
 
-  componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/posts/1', {
-        method: 'GET'
-    })
-    .then((response) => response.json())
-    .then((response) => {
-      this.setState({ data: response})
-    })
-    .catch((error) => {
-        console.error(error);
-    });
+  // componentDidMount() {
+  //   fetch('https://jsonplaceholder.typicode.com/posts/1', {
+  //       method: 'GET'
+  //   })
+  //   .then((response) => response.json())
+  //   .then((response) => {
+  //     this.setState({ data: response})
+  //   })
+  //   .catch((error) => {
+  //       console.error(error);
+  //   });
+  // }
+
+  // The word “async” before a function means one
+  // simple thing that a function always returns a promise.
+  async componentDidMount() {
+    // Wait for the promise to be settled
+    try {
+      const response = await fetch('https://jsonplaceholder.typicode.com/posts/1', { method: 'GET'})
+      const data = await response.json()
+
+      this.setState({ data })
+    } catch (error) {
+      console.error('error ', error)
+    }
   }
 
   render() {
