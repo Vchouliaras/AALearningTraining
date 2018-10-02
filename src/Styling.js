3af158bb1e82bda5e506e7fb32e72d3b9e3a268a
Vasileios Chouliaras <vasilis.chouliaras@gmail.com>
2018-10-02 15:38:52 +0300

    Adding Linear Gradient component

--- src/Styling.js
+++ src/Styling.js
@@ -1,14 +1,34 @@
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View} from 'react-native'
+
+import LinearGradient from 'react-native-linear-gradient'
 
 class Styling extends Component {
   render() {
     return (
-      <View>
-        <Text>Welcome to React Native styling</Text>
-      </View>
+      <LinearGradient colors={['#4c009f', '#1e4998', '#192f33']} style={styles.linearGradient}>
+        <Text style={styles.buttonText}>
+          Sign in with Facebook
+        </Text>
+      </LinearGradient>
     )
   }
 }
+
+var styles = StyleSheet.create({
+  linearGradient: {
+    paddingLeft: 15,
+    paddingRight: 15,
+    borderRadius: 5
+  },
+  buttonText: {
+    fontSize: 18,
+    fontFamily: 'Gill Sans',
+    textAlign: 'center',
+    margin: 10,
+    color: '#ffffff',
+    backgroundColor: 'transparent',
+  },
+});
 
 export default Styling
