1ebc54c914bd6917f6ea351dcb58fcbb453e1c2b
Vasileios Chouliaras <vasilis.chouliaras@gmail.com>
2018-10-02 15:49:57 +0300

    Make and http request

--- src/Styling.js
+++ src/Styling.js
@@ -1,14 +1,39 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 
-class Styling extends Component {
+class HttpExample extends Component {
+
+  state = {data: ''}
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/posts/1', {
+        method: 'GET'
+    })
+    .then((response) => response.json())
+    .then((response) => {
+      this.setState({ data: response})
+    })
+    .catch((error) => {
+        console.error(error);
+    });
+  }
+
   render() {
+
+    if (!this.state.data) {
+      return (
+        <View>
+          <Text>Loding ...</Text>
+        </View>
+      )
+    }
+
     return (
       <View>
-        <Text>Welcome to React Native styling</Text>
+        <Text>{ this.state.data.body }</Text>
       </View>
     )
   }
 }
 
-export default Styling
+export default HttpExample
