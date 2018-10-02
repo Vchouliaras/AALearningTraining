811f3f2eb3eaaf253dbbe7cef14b34be0806db80
Vasileios Chouliaras <vasilis.chouliaras@gmail.com>
2018-10-02 13:18:38 +0300

    Simple text styles

--- src/Styling.js
+++ src/Styling.js
@@ -1,14 +1,32 @@
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 class Styling extends Component {
   render() {
     return (
       <View>
-        <Text>Welcome to React Native styling</Text>
+        <Text style={styles.normal}>Normal Text</Text>
+        <Text style={styles.big}>Big Text</Text>
+        <Text style={styles.bold}>Bold Text</Text>
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  normal: {
+    fontSize: 16,
+    color: 'green'
+  },
+  big: {
+    fontSize: 30,
+    color: 'blue'
+  },
+  bold: {
+    fontSize: 30,
+    color: 'yellow',
+    fontWeight: '800'
+  }
+})
 
 export default Styling
